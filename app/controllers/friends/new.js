5054b3a68960eeda2346045a73f1f19b7a1548ee
Elton Murillo <emurillo510@gmail.com>
2015-02-19 00:45:53 -0800

    Add validation friends/new

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -6,11 +6,38 @@ export default Ember.Controller.extend({
 
 	   save: function(){
 		console.log('+- you have clicked saved.');
+		if(this.get('isValid')){
+		   var _this = this;
+		   this.get('model').save().then(function(friend){
+
+			_this.transitionToRoute('friends.show', friend);
+
+		});
+
+		}else{
+  		   this.set('errorMessage', 'You have to fill all fields!');
+		}
 		return false;
 	   }, 
 	   cancel: function(){
 	       console.log('+- you have clicked cancel.');
 	       return false;	
 	   }
-	}	
+	},
+	isValid: Ember.computed(
+
+	'model.firstName',
+	'model.lastName',
+	'model.email',
+	'model.twitter',
+	function(){
+
+	return !Ember.isEmpty(this.get('model.firstName')) &&
+		!Ember.isEmpty(this.get('model.lastName')) &&
+		 !Ember.isEmpty(this.get('model.email')) &&
+		  !Ember.isEmpty(this.get('model.twitter'));
+
+	
+	})
+
 });
