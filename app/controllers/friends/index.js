b74fe33f58891936f46dae2528d4ab6f9e0add80
Elton Murillo <emurillo510@gmail.com>
2015-02-19 01:38:47 -0800

    Add delete friends feature

---
+++ app/controllers/friends/index.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	actions: {
+
+	   delete: function(friend){
+	      console.log('+- clicked delete in index route');
+	      var _this = this;
+	
+	      friend.destroyRecord().then(function(){
+		_this.transitionToRoute('friends.index');
+	      });
+	      return false;
+    	   }
+        
+	}
+})
