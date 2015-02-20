6e81fe4e6b71a7ff9fd4c2b09bce3a3c05a89723
Elton Murillo <emurillo510@gmail.com>
2015-02-20 01:47:15 -0800

    Add nested route for articles under friends. Add new article feature. Display Articlesunder friends/show

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  email: DS.attr('string'),
-  twitter: DS.attr('string'),
-  totalArticles: DS.attr('number')
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    email: DS.attr('string'),
+    twitter: DS.attr('string'),
+    totalArticles: DS.attr('number'),
+    articles: DS.hasMany('article') //adds relationship
 });
